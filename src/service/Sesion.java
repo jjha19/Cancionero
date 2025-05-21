ee31d53c697f389171729b4fcfe0db0b28dd30b5
Juancito <jjha19@educamadrid.org>
2025-05-21 13:08:45 +0200

    Funciona reproducir cancion. Adicion de menúes

--- src/service/Sesion.java
+++ src/service/Sesion.java
@@ -1,24 +1,4 @@
 package service;
 
-import Common.Constantes;
-import domain.Usuario;
-import ui.EntradaSalida;
-
-public class Sesion {
-    public static Usuario gestionarUsuario(int opcion) {
-        Usuario user = null;
-        if (opcion == 1) {
-            System.out.println(Constantes.PIDEROL);
-            int rol = EntradaSalida.lectorDeOpcionesNumericas();
-            System.out.println(Constantes.PIDEUSERNAME);
-            String username = EntradaSalida.lectorDeTexto();
-            System.out.println(Constantes.PIDECONTRASEÑA);
-            String password = EntradaSalida.lectorDeTexto();
-
-            user = new Usuario()
-        } else if (opcion==2) {
-
-        }
-        return user;
-    }
+public interface Sesion {
 }
